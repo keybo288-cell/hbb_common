9de771a659ae0c864a0fe9e284370a9936c6399a
keybo288-cell <keybo288@gmail.com>
2025-09-10 20:38:23 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["al.keybo.xyz"];
-pub const RS_PUB_KEY: &str = "ylYY47sSeVOx3feVNTJnxujgqp73n6N4HjGTudXa7Rw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["39.97.42.82"];   #修改里面的ip
+pub const RS_PUB_KEY: &str = "yf1hSxAYhq5cBeV3axAQS3guAqI774N5s2LFOGLovcQ=";   #修改里面的密钥
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
